684a31ccf4ab276fd439aa56f95419c75c8494bf
Lennart Schneider <lennart.sch@web.de>
2022-09-27 12:26:19 +0200

    update search spaces

--- attic/benchmarks/search_spaces.R
+++ attic/benchmarks/search_spaces.R
@@ -1,8 +1,33 @@
+# old 19.09.2022
+#get_xgboost_search_space_gagga = function() {
+#  ps(
+#    classif.xgboost.nrounds = p_dbl(lower = log(1), upper = log(5000), tags = c("int", "log"),
+#                                    trafo = function(x) as.integer(round(exp(x))), default = log(500)),
+#    classif.xgboost.eta = p_dbl(lower = log(1e-4), upper = 0, tags = "log",
+#                                trafo = function(x) exp(x), default = log(0.3)),
+#    classif.xgboost.gamma = p_dbl(lower = log(1e-4), upper = log(7), tags = "log",
+#                                  trafo = function(x) exp(x), default = log(1e-4)),
+#    classif.xgboost.lambda = p_dbl(lower = log(1e-4), upper = log(1000), tags = "log",
+#                                   trafo = function(x) exp(x), default = log(1)),
+#    classif.xgboost.alpha = p_dbl(lower = log(1e-4), upper = log(1000), tags = "log",
+#                                  trafo = function(x) exp(x), default = log(1e-4)),
+#    classif.xgboost.subsample = p_dbl(lower = 0.1, upper = 1, default = 1),
+#    classif.xgboost.max_depth = p_int(lower = 1L, upper = 15L, default = 6L),
+#    classif.xgboost.min_child_weight = p_dbl(lower = log(exp(1)), upper = log(150), tags = "log",
+#                                             trafo = function(x) exp(x), default = log(exp(1))),
+#    classif.xgboost.colsample_bytree = p_dbl(lower = 0.01, upper = 1, default = 1),
+#    classif.xgboost.colsample_bylevel = p_dbl(lower = 0.01, upper = 1, default = 1),
+#    select.selector = p_uty(),  # must be part of the search space
+#    classif.xgboost.interaction_constraints = p_uty(),  # must be part of the search space
+#    classif.xgboost.monotone_constraints = p_uty()  # must be part of the search space
+#  )
+#}
+
 get_xgboost_search_space_gagga = function() {
   ps(
     classif.xgboost.nrounds = p_dbl(lower = log(1), upper = log(5000), tags = c("int", "log"),
                                     trafo = function(x) as.integer(round(exp(x))), default = log(500)),
-    classif.xgboost.eta = p_dbl(lower = log(1e-4), upper = 0, tags = "log",
+    classif.xgboost.eta = p_dbl(lower = log(1e-4), upper = log(1), tags = "log",
                                 trafo = function(x) exp(x), default = log(0.3)),
     classif.xgboost.gamma = p_dbl(lower = log(1e-4), upper = log(7), tags = "log",
                                   trafo = function(x) exp(x), default = log(1e-4)),
@@ -11,8 +36,8 @@ get_xgboost_search_space_gagga = function() {
     classif.xgboost.alpha = p_dbl(lower = log(1e-4), upper = log(1000), tags = "log",
                                   trafo = function(x) exp(x), default = log(1e-4)),
     classif.xgboost.subsample = p_dbl(lower = 0.1, upper = 1, default = 1),
-    classif.xgboost.max_depth = p_int(lower = 1L, upper = 15L, default = 6L),
-    classif.xgboost.min_child_weight = p_dbl(lower = log(exp(1)), upper = log(150), tags = "log",
+    classif.xgboost.max_depth = p_int(lower = 1L, upper = 20L, default = 6L),
+    classif.xgboost.min_child_weight = p_dbl(lower = log(1), upper = log(150), tags = "log",
                                              trafo = function(x) exp(x), default = log(exp(1))),
     classif.xgboost.colsample_bytree = p_dbl(lower = 0.01, upper = 1, default = 1),
     classif.xgboost.colsample_bylevel = p_dbl(lower = 0.01, upper = 1, default = 1),
@@ -22,11 +47,33 @@ get_xgboost_search_space_gagga = function() {
   )
 }
 
+# old 19.09.2022
+#get_xgboost_search_space = function() {
+#  ps(
+#    classif.xgboost.nrounds = p_dbl(lower = log(1), upper = log(5000), tags = c("int", "log"),
+#                                    trafo = function(x) as.integer(round(exp(x))), default = log(500)),
+#    classif.xgboost.eta = p_dbl(lower = log(1e-4), upper = 0, tags = "log",
+#                                trafo = function(x) exp(x), default = log(0.3)),
+#    classif.xgboost.gamma = p_dbl(lower = log(1e-4), upper = log(7), tags = "log",
+#                                  trafo = function(x) exp(x), default = log(1e-4)),
+#    classif.xgboost.lambda = p_dbl(lower = log(1e-4), upper = log(1000), tags = "log",
+#                                   trafo = function(x) exp(x), default = log(1)),
+#    classif.xgboost.alpha = p_dbl(lower = log(1e-4), upper = log(1000), tags = "log",
+#                                  trafo = function(x) exp(x), default = log(1e-4)),
+#    classif.xgboost.subsample = p_dbl(lower = 0.1, upper = 1, default = 1),
+#    classif.xgboost.max_depth = p_int(lower = 1L, upper = 15L, default = 6L),
+#    classif.xgboost.min_child_weight = p_dbl(lower = log(exp(1)), upper = log(150), tags = "log",
+#                                             trafo = function(x) exp(x), default = log(exp(1))),
+#    classif.xgboost.colsample_bytree = p_dbl(lower = 0.01, upper = 1, default = 1),
+#    classif.xgboost.colsample_bylevel = p_dbl(lower = 0.01, upper = 1, default = 1)
+#  )
+#}
+
 get_xgboost_search_space = function() {
   ps(
     classif.xgboost.nrounds = p_dbl(lower = log(1), upper = log(5000), tags = c("int", "log"),
                                     trafo = function(x) as.integer(round(exp(x))), default = log(500)),
-    classif.xgboost.eta = p_dbl(lower = log(1e-4), upper = 0, tags = "log",
+    classif.xgboost.eta = p_dbl(lower = log(1e-4), upper = log(1), tags = "log",
                                 trafo = function(x) exp(x), default = log(0.3)),
     classif.xgboost.gamma = p_dbl(lower = log(1e-4), upper = log(7), tags = "log",
                                   trafo = function(x) exp(x), default = log(1e-4)),
@@ -35,8 +82,8 @@ get_xgboost_search_space = function() {
     classif.xgboost.alpha = p_dbl(lower = log(1e-4), upper = log(1000), tags = "log",
                                   trafo = function(x) exp(x), default = log(1e-4)),
     classif.xgboost.subsample = p_dbl(lower = 0.1, upper = 1, default = 1),
-    classif.xgboost.max_depth = p_int(lower = 1L, upper = 15L, default = 6L),
-    classif.xgboost.min_child_weight = p_dbl(lower = log(exp(1)), upper = log(150), tags = "log",
+    classif.xgboost.max_depth = p_int(lower = 1L, upper = 20L, default = 6L),
+    classif.xgboost.min_child_weight = p_dbl(lower = log(1), upper = log(150), tags = "log",
                                              trafo = function(x) exp(x), default = log(exp(1))),
     classif.xgboost.colsample_bytree = p_dbl(lower = 0.01, upper = 1, default = 1),
     classif.xgboost.colsample_bylevel = p_dbl(lower = 0.01, upper = 1, default = 1)
@@ -78,18 +125,19 @@ get_glmnet_search_space = function() {
   )
 }
 
-get_xgboost_domain = function() {
-  ps(
-    classif.xgboost.nrounds = p_int(lower = 1L, upper = 5000L, default = 500L),
-    classif.xgboost.eta = p_dbl(lower = 1e-4, upper = 1, default = 0.3),
-    classif.xgboost.gamma = p_dbl(lower = 1e-4, upper = 7, default = 1e-4),
-    classif.xgboost.lambda = p_dbl(lower = 1e-4, upper = 1000, default = 1),
-    classif.xgboost.alpha = p_dbl(lower = 1e-4, upper = 1000, default = 1e-4),
-    classif.xgboost.subsample = p_dbl(lower = 0.1, upper = 1, default = 1),
-    classif.xgboost.max_depth = p_int(lower = 1L, upper = 15L, default = 6L),
-    classif.xgboost.min_child_weight = p_dbl(lower = exp(1), upper = 150, default = exp(1)),
-    classif.xgboost.colsample_bytree = p_dbl(lower = 0.01, upper = 1, default = 1),
-    classif.xgboost.colsample_bylevel = p_dbl(lower = 0.01, upper = 1, default = 1)
-  )
-}
+# FIXME:
+#get_xgboost_domain = function() {
+#  ps(
+#    classif.xgboost.nrounds = p_int(lower = 1L, upper = 5000L, default = 500L),
+#    classif.xgboost.eta = p_dbl(lower = 1e-4, upper = 1, default = 0.3),
+#    classif.xgboost.gamma = p_dbl(lower = 1e-4, upper = 7, default = 1e-4),
+#    classif.xgboost.lambda = p_dbl(lower = 1e-4, upper = 1000, default = 1),
+#    classif.xgboost.alpha = p_dbl(lower = 1e-4, upper = 1000, default = 1e-4),
+#    classif.xgboost.subsample = p_dbl(lower = 0.1, upper = 1, default = 1),
+#    classif.xgboost.max_depth = p_int(lower = 1L, upper = 15L, default = 6L),
+#    classif.xgboost.min_child_weight = p_dbl(lower = exp(1), upper = 150, default = exp(1)),
+#    classif.xgboost.colsample_bytree = p_dbl(lower = 0.01, upper = 1, default = 1),
+#    classif.xgboost.colsample_bylevel = p_dbl(lower = 0.01, upper = 1, default = 1)
+#  )
+#}
 
